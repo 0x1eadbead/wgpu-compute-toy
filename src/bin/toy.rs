4ca9c412b656d58912c189739c8ebfc22b430d02
0x1eadbead <61933856+0x1eadbead@users.noreply.github.com>
2023-08-13 22:03:24 +0200

    Revert "refactor watcher a bit"
    This reverts commit 4c34ff8401bf11550a92637ac17fc5014db5e642.

--- src/bin/toy.rs
+++ src/bin/toy.rs
@@ -4,7 +4,7 @@ use wgputoy::context::init_wgpu;
 use wgputoy::WgpuToyRenderer;
 use wgputoy::config;
 use std::sync::{Arc, Mutex};
-use notify::{self, Config, RecommendedWatcher, RecursiveMode};
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -78,91 +78,16 @@ async fn init(config: &config::Config) -> Result<WgpuToyRenderer, Box<dyn Error>
     Ok(wgputoy)
 }
 
-struct WatcherState<T> {
-    value: T,
+#[derive(Clone, Default)]
+struct Shader {
+    text: String,
     generation: u64,
 }
 
-struct Watcher<T>
-where T: Send + 'static
-{
-    state: Arc<Mutex<WatcherState<T>>>,
-}
-
-struct Watch<T>
-where T: Send + 'static
-{
-    state: Arc<Mutex<WatcherState<T>>>,
-    pub generation: u64,
-}
-
-impl<T> Watch<T>
-where T: Send + Clone + 'static
-{
-    pub fn check(&mut self) -> Option<T> {
-        let state = self.state.lock().unwrap();
-        if state.generation > self.generation {
-            self.generation = state.generation;
-            Some(state.value.clone())
-        } else {
-            None
-        }
-    }
-}
-
-impl<T> Watcher<T>
-where T: Send + 'static
-{
-    pub fn new(value: T) -> Self {
-        Self {
-            state: Arc::new(Mutex::new(
-                WatcherState {
-                    value,
-                    generation: 1,
-                }
-            )),
-        }
-    }
-
-    pub fn subscribe(&self) -> Watch<T> {
-        let state = self.state.lock().unwrap();
-        Watch {
-            state: self.state.clone(),
-            generation: state.generation - 1,
-        }
-    }
-
-    pub fn run<F>(&self, mut f: F, filename: String) -> std::thread::JoinHandle<()>
-    where F: FnMut(&str) -> Option<T> + Send + 'static
-    {
-        let state_clone = self.state.clone();
-        std::thread::spawn(move || {
-            use notify::Watcher;
-            let (tx, rx) = std::sync::mpsc::channel();
-            let mut watcher = RecommendedWatcher::new(tx, notify::Config::default()).unwrap();
-            watcher.watch(filename.as_ref(), RecursiveMode::NonRecursive).unwrap();
-
-            for res in rx {
-                if res.is_err() {
-                    continue;
-                }
-
-                let res = res.unwrap();
-
-                if let notify::EventKind::Modify(_) = res.kind {
-
-                    if let Ok(text) = std::fs::read_to_string(&filename) {
-                        if let Some(value) = f(&text) {
-                            let mut state = state_clone.lock().unwrap();
-                            state.value = value;
-                            state.generation += 1;
-                        }
-                    };
-                }
-            }
-        })
-    }
-
+#[derive(Clone)]
+struct SharedConfig {
+    config: config::Config,
+    generation: u64
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -187,28 +112,76 @@ fn main() -> Result<(), Box<dyn Error>> {
     let event_loop = std::mem::take(&mut wgputoy.wgpu.event_loop).unwrap();
     let device_clone = wgputoy.wgpu.device.clone();
 
+    let mut new_shader: Arc<Mutex<Shader>> = Arc::new(Default::default());
+    let new_shader_clone = new_shader.clone();
+
     let shader_path = config.shader_path.clone();
 
-    let shader_watcher = Watcher::<String>::new(std::fs::read_to_string(&shader_path).unwrap());
-    let _shader_watcher_thread = shader_watcher.run(|text| {
-        Some(text.into())
-    }, shader_path);
+    // shader watcher
+    std::thread::spawn(move || loop {
+        let filename = shader_path.clone();
 
-    let config_watcher = Watcher::new(config);
-    let _config_watcher_thread = config_watcher.run(|text| {
-        if let Ok(config) = serde_json::from_str(&text) {
-            Some(config)
-        } else {
-            None
+        let (tx, rx) = std::sync::mpsc::channel();
+        let mut watcher = RecommendedWatcher::new(tx, Config::default()).unwrap();
+        watcher.watch(filename.as_ref(), RecursiveMode::NonRecursive).unwrap();
+
+        for res in rx {
+            if res.is_err() {
+                continue;
+            }
+
+            let res = res.unwrap();
+
+            if let notify::EventKind::Modify(_) = res.kind {
+
+                let _ = std::fs::read_to_string(&filename).map(|text|
+                {
+                    let mut shared_shader = new_shader_clone.lock().unwrap();
+                    shared_shader.generation += 1;
+                    shared_shader.text = text;
+                });
+            }
         }
-    }, config_path.clone());
+    });
 
+    let mut new_config: Arc<Mutex<SharedConfig>> = Arc::new(Mutex::new(
+        SharedConfig { config: config.clone(), generation: 1 } // generation hack to always apply on first frame
+    ));
+    let new_config_clone = new_config.clone();
+
+    std::thread::spawn(move || loop {
+        let filename = config_path.clone();
+
+        let (tx, rx) = std::sync::mpsc::channel();
+        let mut watcher = RecommendedWatcher::new(tx, Config::default()).unwrap();
+        watcher.watch(filename.as_ref(), RecursiveMode::NonRecursive).unwrap();
+
+        for res in rx {
+            if res.is_err() {
+                continue;
+            }
+
+            let res = res.unwrap();
+
+            if let notify::EventKind::Modify(_) = res.kind {
+
+                let _ = std::fs::read_to_string(&filename).map(|text|
+                {
+                    if let Ok(config) = serde_json::from_str(&text) {
+                        let mut shared_config = new_config_clone.lock().unwrap();
+                        shared_config.generation += 1;
+                        shared_config.config = config;
+                    }
+                });
+            }
+        }
+    });
     std::thread::spawn(move || loop {
         device_clone.poll(wgpu::Maintain::Wait);
     });
 
-    let mut shader_updates = shader_watcher.subscribe();
-    let mut config_updates = config_watcher.subscribe();
+    let mut last_shader_generation: u64 = 0;
+    let mut last_config_generation: u64 = 0;
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = winit::event_loop::ControlFlow::Poll;
@@ -216,11 +189,23 @@ fn main() -> Result<(), Box<dyn Error>> {
             winit::event::Event::RedrawRequested(_) => {
                 let time = start_time.elapsed().as_micros() as f32 * 1e-6;
 
-                if let Some(new_config) = config_updates.check() {
-                    wgputoy.update(&new_config);
+                if let Ok(shared_config) = new_config.lock() {
+                    if shared_config.generation > last_config_generation {
+                        wgputoy.update(&shared_config.config);
+                        last_config_generation = shared_config.generation;
+                    }
                 }
 
-                if let Some(text) = shader_updates.check() {
+                let mut new_text = Option::None;
+
+                if let Ok(shader) = new_shader.lock() {
+                    if shader.generation > last_shader_generation {
+                        last_shader_generation = shader.generation;
+                        new_text = Some(shader.text.clone());
+                    }
+                }
+
+                if let Some(text) = new_text {
                     runtime.block_on(wgputoy.preprocess_async(&text)).map(|preprocessed| {
                         let result = std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| {
                             wgputoy.compile(preprocessed);
